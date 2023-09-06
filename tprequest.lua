2cb14c57710a1c2b8e13d0281b016df809378399
CoralDev0 <116958279+CoralDev0@users.noreply.github.com>
2023-09-07 07:24:47 +0800

    tf.sh

--- tprequest.lua
+++ tprequest.lua
@@ -1,26 +1,39 @@
-local HttpService = game:GetService("HttpService")
-local RunService = game:GetService("RunService")
+-- Created by YouTube: MrGabbe. Discord: Mr_G_Curry#6982
+local mouse = game.Players.LocalPlayer:GetMouse()
+local tool = Instance.new("Tool")
+tool.RequiresHandle = false
+tool.Name = "the most useful tool in the world" -- yes dis is true
 
-local scriptUrls = {
-    "https://www.example.com/script1.lua",
-    "https://www.example.com/script2.lua",
-    -- Add more script URLs as needed
-}
+local hpt = Instance.new("Part", tool)
+hpt.Size = Vector3.new(1, 1, 1)
+hpt.Anchored = false
+hpt.CanCollide = false
+hpt.Transparency = 1
 
-local function importScripts()
-    for _, url in pairs(scriptUrls) do
-        local success, scriptCode = pcall(function()
-            return HttpService:GetAsync(url)
-        end)
+tool.Activated:Connect(function()
+    local character = game.Players.LocalPlayer.Character
+    if character then
+        local humanoidRootPart = character:FindFirstChild("HumanoidRootPart")
+        if humanoidRootPart then
+            local ps = mouse.Hit.p + Vector3.new(0, 2.5, 0)
+            humanoidRootPart.CFrame = CFrame.new(ps)
+        end
+    end
+end)
 
-        if success then
-            local script = Instance.new("Script")
-            script.Source = scriptCode
-            script.Parent = game.Workspace -- Or wherever you want to put the imported scripts
-        else
-            warn("Failed to import script from URL:", url)
+tool.Parent = game.Players.LocalPlayer.Backpack
+
+local runService = game:GetService("RunService")
+local function updateHptPosition()
+    local character = game.Players.LocalPlayer.Character
+    if character then
+        local handleAttachment = tool:FindFirstChild("Handle")  -- Assuming the tool has a "Handle" attachment
+        if handleAttachment then
+            local handlePosition = handleAttachment.WorldPosition
+            hpt.CFrame = CFrame.new(handlePosition)
         end
     end
 end
 
-RunService.Heartbeat:Connect(importScripts)
+runService.RenderStepped:Connect(updateHptPosition)
+-- this will teleport u if u hold da tool
