c407f97a12f1b08ee9537ef41817bb2665f3544c
CoralDev0 <116958279+CoralDev0@users.noreply.github.com>
2023-09-10 14:49:11 +0800

    Update Gph call.lua

--- Gph call.lua
+++ Gph call.lua
@@ -1,20 +1,38 @@
+-- gphc or roblox fsf (functional script finder) is a script finder that is not that important /: it acts like a loadstring(game:HttpGet) Stuff, for example gph.can("www.github.com/guipro/potatoes%20are%20good%20like%20google%20(:", "the name of the script")
+
+-- Define the gph module
 local gph = {}
 
+-- gph.can function for loading and executing remote scripts
 function gph.can(Loadstring, Chunkname)
-    loadstring(game:HttpGet(Loadstring, Chunkname))()
+    local success, scriptFunction = pcall(function()
+        return loadstring(game:HttpGet(Loadstring), Chunkname)
+    end)
+    
+    if success then
+        scriptFunction()  -- Execute the loaded script
+    else
+        warn("Failed to load and execute the script:", scriptFunction)
+    end
 end
 
+-- hello C:
+
+-- Define the rem module for Roblox services
 local rem = {}
 
+-- rem.getservice function to get a Roblox service by name
 function rem.getservice(ServiceName)
     return game:GetService(ServiceName)
 end
-
-local remdat -- Define the variable
 
 -- Example of using rem.getservice() to get the Workspace service:
 local workspaceService = rem.getservice("Workspace")
 print(workspaceService)
 
-remdat = "Remdat,robloxmemmory", _G.Memmory
+-- Define remdat variable (assuming it's for data related to rem)
+local remdat = {
+    "Remdat",  -- Example string
+    _G.Memmory -- Assuming _G.Memmory is another variable you want to include
+}
 print(remdat)
